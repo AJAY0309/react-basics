6997b0d8eb8882ec1057a5ce7234431e53c63c54
AJAY0309 <98105667+AJAY0309@users.noreply.github.com>
2023-08-19 13:52:15 +0530

    first

---
+++ react-basics/src/CBC.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class CBC extends Component {
+  render() {
+    return (
+      <div>
+        <p>This is class based compontnt</p>
+      </div>
+    )
+  }
+}
---
+++ react-basics/src/FBC.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function FBC() {
+  return (
+    <div>
+      <p>This is function base component</p>
+    </div>
+  )
+}
